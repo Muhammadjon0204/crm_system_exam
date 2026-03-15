d01a0a6ecfdefc1934b56ce74eb5e84304c35c3f
Muhammadjon0204 <kosimovmuhamadjon23@gmail.com>
2026-03-15 14:46:06 +0300

    Clean up: remove logs, bin, obj, ide files

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6b482af5805a444e651a46fdc81d31246c1d51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8961d808ced012e51d4f845ed060ac45c39426f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
